54778bba1c7dac103d9f917b7e1798d096962823
bart_dude <kanye@seek.li>
2026-04-28 04:18:44 +0800

    boot_qemu.sh

---
+++ boot_qemu.sh
@@ -0,0 +1,12 @@
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+boot_qemu() {
+  exec qemu-system-x86_64 \
+    -m 1G \
+    -enable-kvm \
+    -kernel "${SCRIPT_DIR}/bzImage" \
+    -initrd "${SCRIPT_DIR}/initrd.img" \
+    -nographic \
+    -append "console=ttyS0 nokaslr"
+}
+boot_qemu
+
